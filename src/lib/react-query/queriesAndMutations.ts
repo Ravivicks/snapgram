baa89ed8d42efb230e043921df8580775c22ae34
Ravivicks <109452041+Ravivicks@users.noreply.github.com>
2024-06-22 13:13:34 +0530

    Update queriesAndMutations.ts

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -173,20 +173,6 @@ export const useDeletePost = () => {
   });
 };
 
-export const useGetPosts = () => {
-  return useInfiniteQuery({
-    queryKey: [QUERY_KEYS.GET_INFINITE_POSTS],
-    queryFn: getInfinitePosts,
-    getNextPageParam: (lastPage) => {
-      if (lastPage && lastPage?.documents.length === 0) {
-        return null;
-      }
-      const lastId = lastPage?.documents[lastPage?.documents.length - 1].$id;
-      return lastId;
-    },
-  });
-};
-
 export const useSearchPosts = (searchTerm: string) => {
   return useQuery({
     queryKey: [QUERY_KEYS.SEARCH_POSTS, searchTerm],
